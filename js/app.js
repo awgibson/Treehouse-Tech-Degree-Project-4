aae8189248551eb54ce5015cdcf33d729386cfff
awgibson <34604592+awgibson@users.noreply.github.com>
2018-08-11 13:22:34 -0500

    wrapped in a function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-const body = document.getElementsByTagName('BODY');
-let board;
-let player1;
-let player2;
+(function () {
+    const body = document.getElementsByTagName('BODY');
+    let board;
+    let player1;
+    let player2;
 
-class Board {
-    constructor() {
-        this._board = [
-            [],
-            [],
-            []
-        ];
-        this._boardHTML = `<div class="board" id="board">
+    class Board {
+        constructor() {
+            this._board = [
+                [],
+                [],
+                []
+            ];
+            this._boardHTML = `<div class="board" id="board">
         <header>
           <h1>Tic Tac Toe</h1>
           <ul>
@@ -32,37 +33,37 @@ class Board {
         </div>`;
 
 
-    }
-    loadGame(visible) {
-        body[0].innerHTML = this._boardHTML;
-        const start = document.getElementById('start');
-        const player1 = document.getElementById('player1');
-        const boardSpaces = document.querySelectorAll('.box');
-        player1.classList.add('active');
-
-        for (let i = 0; i < boardSpaces.length; i++) {
-            boardSpaces[i].id = i;
         }
-    }
+        loadGame(visible) {
+            body[0].innerHTML = this._boardHTML;
+            const start = document.getElementById('start');
+            const player1 = document.getElementById('player1');
+            const boardSpaces = document.querySelectorAll('.box');
+            player1.classList.add('active');
 
-    updateBoard(event, player) {
-        const space = event.target.id;
-        if (space <= 2) {
-            this._board[0][space] = player._letter;
-            this.checkWin(player);
-        } else if (space >= 3 && space <= 5) {
-            this._board[1][space - 3] = player._letter;
-            this.checkWin(player);
-        } else if (space >= 6 && space <= 8) {
-            this._board[2][space - 6] = player._letter;
-            this.checkWin(player);
+            for (let i = 0; i < boardSpaces.length; i++) {
+                boardSpaces[i].id = i;
+            }
         }
 
+        updateBoard(event, player) {
+            const space = event.target.id;
+            if (space <= 2) {
+                this._board[0][space] = player._letter;
+                this.checkWin(player);
+            } else if (space >= 3 && space <= 5) {
+                this._board[1][space - 3] = player._letter;
+                this.checkWin(player);
+            } else if (space >= 6 && space <= 8) {
+                this._board[2][space - 6] = player._letter;
+                this.checkWin(player);
+            }
 
-    }
 
-    winScreen(player, isTie = false) {
-        const win = `<div class="screen screen-win" id="finish">
+        }
+
+        winScreen(player, isTie = false) {
+            const win = `<div class="screen screen-win" id="finish">
         <header>
           <h1>Tic Tac Toe</h1>
           <p class="message"></p>
@@ -70,174 +71,175 @@ class Board {
         </header>
       </div>`;
 
-        body[0].innerHTML = win;
-        const winScreen = document.getElementById('finish');
-        const message = document.getElementsByClassName('message');
+            body[0].innerHTML = win;
+            const winScreen = document.getElementById('finish');
+            const message = document.getElementsByClassName('message');
 
-        if (player._isWinner === true) {
-            winScreen.classList.add(player._winClass);
-            message[0].innerText = 'Winner!';
-        } else {
-            winScreen.classList.add('screen-win-tie');
-            message[0].innerText = 'Tie!';
+            if (player._isWinner === true) {
+                winScreen.classList.add(player._winClass);
+                message[0].innerText = 'Winner!';
+            } else {
+                winScreen.classList.add('screen-win-tie');
+                message[0].innerText = 'Tie!';
+            }
+
         }
 
-    }
+        checkWin(player) {
+            for (let i = 0; i < 3; i++) {
+                //check horizontal wins
+                if (player._letter === this._board[i][0] &&
+                    player._letter === this._board[i][1] &&
+                    player._letter === this._board[i][2]) {
+                    player._isWinner = true;
+                }
+                // //check vertical wins
+                if (player._letter === this._board[0][i] &&
+                    player._letter === this._board[1][i] &&
+                    player._letter === this._board[2][i]) {
+                    player._isWinner = true;
+                }
+            }
 
-    checkWin(player) {
-        for (let i = 0; i < 3; i++) {
-            //check horizontal wins
-            if (player._letter === this._board[i][0] &&
-                player._letter === this._board[i][1] &&
-                player._letter === this._board[i][2]) {
+            //check wins on the diagonal
+            if (player._letter === this._board[0][0] &&
+                player._letter === this._board[1][1] &&
+                player._letter === this._board[2][2]) {
                 player._isWinner = true;
             }
-            // //check vertical wins
-            if (player._letter === this._board[0][i] &&
-                player._letter === this._board[1][i] &&
-                player._letter === this._board[2][i]) {
+
+            if (player._letter === this._board[0][2] &&
+                player._letter === this._board[1][1] &&
+                player._letter === this._board[2][0]) {
                 player._isWinner = true;
+            }
+
+            if (player._isWinner) {
+                this.winScreen(player);
+            } else if (this._board.toString().length === 17) {
+                this.winScreen(player);
+            }
+
+        }
+
+        hoverOn(event, currentPlayer) {
+            if (currentPlayer === 'player1') {
+                event.target.style.backgroundImage = player1.img;
+            } else if (currentPlayer === 'player2') {
+                event.target.style.backgroundImage = player2.img;
+            } else {
+                return false;
             }
         }
 
-        //check wins on the diagonal
-        if (player._letter === this._board[0][0] &&
-            player._letter === this._board[1][1] &&
-            player._letter === this._board[2][2]) {
-            player._isWinner = true;
-        }
-
-        if (player._letter === this._board[0][2] &&
-            player._letter === this._board[1][1] &&
-            player._letter === this._board[2][0]) {
-            player._isWinner = true;
-        }
-
-        if (player._isWinner) {
-            this.winScreen(player);
-        } else if (this._board.toString().length === 17) {
-            this.winScreen(player);
+        hoverOut(event, currentPlayer) {
+            if (currentPlayer === 'player1') {
+                event.target.style.backgroundImage = '';
+            } else if (currentPlayer === 'player2') {
+                event.target.style.backgroundImage = '';
+            } else {
+                return false;
+            }
         }
 
     }
 
-    hoverOn(event, currentPlayer) {
-        if (currentPlayer === 'player1') {
-            event.target.style.backgroundImage = player1.img;
-        } else if (currentPlayer === 'player2') {
-            event.target.style.backgroundImage = player2.img;
-        } else {
-            return false;
+    class Player {
+        constructor(player, isComputer = false) {
+            this._player = player;
+            this._isComputer = isComputer;
+            this._playerActiveGraphic = document.getElementById(this._player);
+            this._isWinner = false;
+
+            if (this._player === 'player1') {
+                this._isTurn = true;
+                this._img = `url('img/o.svg')`;
+                this._letter = 'o';
+                this._playClass = 'box-filled-1';
+                this._winClass = 'screen-win-one';
+            } else {
+                this._isTurn = false;
+                this._img = `url('img/x.svg')`;
+                this._letter = 'x';
+                this._playClass = 'box-filled-2';
+                this._winClass = 'screen-win-two';
+            }
         }
-    }
 
-    hoverOut(event, currentPlayer) {
-        if (currentPlayer === 'player1') {
-            event.target.style.backgroundImage = '';
-        } else if (currentPlayer === 'player2') {
-            event.target.style.backgroundImage = '';
-        } else {
-            return false;
+        get img() {
+            return this._img;
         }
-    }
 
-}
-
-class Player {
-    constructor(player, isComputer = false) {
-        this._player = player;
-        this._isComputer = isComputer;
-        this._playerActiveGraphic = document.getElementById(this._player);
-        this._isWinner = false;
-
-        if (this._player === 'player1') {
-            this._isTurn = true;
-            this._img = `url('img/o.svg')`;
-            this._letter = 'o';
-            this._playClass = 'box-filled-1';
-            this._winClass = 'screen-win-one';
-        } else {
-            this._isTurn = false;
-            this._img = `url('img/x.svg')`;
-            this._letter = 'x';
-            this._playClass = 'box-filled-2';
-            this._winClass = 'screen-win-two';
+        set turn(isTurn) {
+            this._isTurn = isTurn;
+            if (this._isTurn === false) {
+                this._playerActiveGraphic.classList.remove('active');
+            }
+            if (this._isTurn === true) {
+                this._playerActiveGraphic.classList.add('active');
+            }
         }
-    }
 
-    get img() {
-        return this._img;
-    }
 
-    set turn(isTurn) {
-        this._isTurn = isTurn;
-        if (this._isTurn === false) {
-            this._playerActiveGraphic.classList.remove('active');
+        move(event) {
+            event.target.classList.add(this._playClass);
+            event.target.classList.add('taken');
+            board.updateBoard(event, this);
+            this.turn = false;
+            if (this._player === 'player1') {
+                player2.turn = true;
+            } else if (this._player === 'player2') {
+                player1.turn = true;
+            } else {
+                return false;
+            }
         }
-        if (this._isTurn === true) {
-            this._playerActiveGraphic.classList.add('active');
-        }
+
     }
 
 
-    move(event) {
-        event.target.classList.add(this._playClass);
-        event.target.classList.add('taken');
-        board.updateBoard(event, this);
-        this.turn = false;
-        if (this._player === 'player1') {
-            player2.turn = true;
-        } else if (this._player === 'player2') {
-            player1.turn = true;
-        } else {
-            return false;
-        }
-    }
-
-}
-
-
-window.addEventListener('load', () => {
-    const start = `<div class="screen screen-start" id="start">
+    window.addEventListener('load', () => {
+        const start = `<div class="screen screen-start" id="start">
     <header>
       <h1>Tic Tac Toe</h1>
       <a href="#" class="button">Start game</a>
     </header>
     </div>`;
-    body[0].innerHTML = start;
-});
+        body[0].innerHTML = start;
+    });
 
-document.addEventListener('mousedown', (event) => {
-    if (event.target.className === 'button') {
-        board = new Board();
-        board.loadGame();
-        player2 = new Player('player2');
-        player1 = new Player('player1');
+    document.addEventListener('mousedown', (event) => {
+        if (event.target.className === 'button') {
+            board = new Board();
+            board.loadGame();
+            player2 = new Player('player2');
+            player1 = new Player('player1');
 
-    } else if (event.target.className === 'box' && event.target.classList.contains('taken') === false) {
-        const currentPlayer = document.querySelector('.active').id;
-        if (currentPlayer === 'player1') {
-            player1.move(event);
-        } else if (currentPlayer === 'player2') {
-            player2.move(event);
+        } else if (event.target.className === 'box' && event.target.classList.contains('taken') === false) {
+            const currentPlayer = document.querySelector('.active').id;
+            if (currentPlayer === 'player1') {
+                player1.move(event);
+            } else if (currentPlayer === 'player2') {
+                player2.move(event);
+            }
         }
-    }
 
-});
+    });
 
-document.addEventListener('mouseover', (event) => {
-    if (event.target.classList.contains('box') && event.target.classList.contains('taken') === false && event.target.tagName === 'LI') {
-        const currentPlayer = document.querySelector('.active').id;
-        board.hoverOn(event, currentPlayer);
-    }
-});
+    document.addEventListener('mouseover', (event) => {
+        if (event.target.classList.contains('box') && event.target.classList.contains('taken') === false && event.target.tagName === 'LI') {
+            const currentPlayer = document.querySelector('.active').id;
+            board.hoverOn(event, currentPlayer);
+        }
+    });
 
-document.addEventListener('mouseout', (event) => {
+    document.addEventListener('mouseout', (event) => {
 
-    if (event.target.classList.contains('box') && event.target.tagName === 'LI' && event.target.classList.contains('taken') === false) {
-        console.log('mouse out!');
-        const currentPlayer = document.querySelector('.active').id;
-        board.hoverOut(event, currentPlayer);
-    }
-});
+        if (event.target.classList.contains('box') && event.target.tagName === 'LI' && event.target.classList.contains('taken') === false) {
+            console.log('mouse out!');
+            const currentPlayer = document.querySelector('.active').id;
+            board.hoverOut(event, currentPlayer);
+        }
+    });
 
+})();
